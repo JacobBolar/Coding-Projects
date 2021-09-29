bbe6641f5fce047ff51c86fd610d5494729dbce2
JacobBolar <90113939+JacobBolar@users.noreply.github.com>
2021-09-29 13:28:17 -0400

    testing duplicate upload

--- PythonGithub/breach_cost.py
+++ PythonGithub/breach_cost.py
@@ -1,6 +1,6 @@
 #Description: Simple elif program to determine cost of data breach based on industry and records
 #Author: Jacob Bolar
-#Date: 18-SEP-2021
+#Date: 29-SEP-2021
 
 print("""To determine the cost of a data breach in your organization, 
 please provide your industry and the number of records breached.
@@ -12,27 +12,46 @@ when asked for 'Industry', please indicate one of the following:
 #Dictionary of Cost amount per industry
 Dict = {1: 316, 2: 286, 3: 259, 4: 237, 5: 236, 6: 223, 7: 219, 8: 209, 9: 204, 
         10: 196, 11: 183, 12: 181, 13: 172, 14: 125, 15: 93, 16: 73}
+        
+industry = ""
+count = 0
+totalCostAll = 0
 
-industry = input("\nPlease indicate the industy: ")
 
 #Method so we can keep elifs clean.  Calculates total cost of breach
 def calcs(Dict):
 	recordsBreached = int(input("Enter the Number of Records Breached: "))
 	totalCost = int(recordsBreached * Dict)
+	global totalCostAll 
+	totalCostAll += int(totalCost)
 	print("Cost of a ",recordsBreached," records data breach in " + industry + " is ",totalCost)
+	global count
+	count += 1
 
-if industry == "Communications":
-	calcs(Dict[7])
+
+while industry != "quit":
 	
-elif industry == "Retail":
-	calcs(Dict[14])
+	industry = input("\nPlease indicate the industry or type 'quit' to exit: ")
 
-elif industry == "Pharmaceutical":
-	calcs(Dict[8])
+	if industry == "Communications":
+		calcs(Dict[7])
+			
+	elif industry == "Retail":
+		calcs(Dict[14])
 
-elif industry == "Education":
-	calcs(Dict[3])
+	elif industry == "Pharmaceutical":
+		calcs(Dict[8])
 
-else:
-	print("Unknown Industry")
+	elif industry == "Education":
+		calcs(Dict[3])
 	
+	elif industry == "quit":
+		print("Calculations Complete!")
+
+	else:
+		print("Unknown Industry")
+
+averageCost = int(totalCostAll/count)
+print("Number of Records Breached: ", count )
+print("Total Cost of Records Breached: ", totalCostAll)
+print("Average cost Per Breach: ", averageCost)
